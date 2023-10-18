71909405f2f4ae460724201ddc1e9bca24febad0
GauravSingh2003 <fox1532003@gmail.com>
2023-10-18 23:26:46 +0530

    Finalize result

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Search-the-longest-compounded-word
+1.Read the input file and the words in a list or array.
+2.Sort the words in descending order of their length. This will help us identify the longest compounded words easily.
+3.Iterate through each word in the sorted list and check if it can be constructed using any shorter words in the same file.
+4.Keep track of the longest and second longest compounded words found so far.
+5.Measure the time taken to process the input file.
--- pythoncode.py
+++ pythoncode.py
@@ -7,7 +7,63 @@ def read_words_from_file(file_name):
 
     return
 
-input_file = 'Input1.txt'
+input_file = 'Input.txt'
+words = read_words_from_file(input_file)
+
+# Step 2: Sort the words in descending order of length
+words.sort(key=len, reverse=True)
+
+import time
+
+# Step 1: Read the input file and store the words
+def read_words_from_file(file_name):
+    with open(file_name, 'r') as file:
+        words = [line.strip() for line in file]
+
+    return
+
+input_file = 'Input_01.txt'
+words = read_words_from_file(input_file)
+
+# Step 2: Sort the words in descending order of length
+words.sort(key=len, reverse=True)
+
+# Step 3, 4: Identify the longest compounded words
+def is_compounded(word, word_set):
+    if word in word_set:
+        return True
+
+    for i in range(1, len(word)):
+        prefix, suffix = word[:i], word[i:]
+        if prefix in word_set and is_compounded(suffix, word_set):
+            return True
+
+    return False
+
+def get_longest_compounded_words(words):
+    longest_word = ""
+    second_longest_word = ""
+    word_set = set(words)
+
+    for word in words:
+        if is_compounded(word, word_set):
+            if len(word) > len(longest_word):
+                second_longest_word = longest_word
+                longest_word = word
+            elif len(word) > len(second_longest_word):
+                second_longest_word = word
+        return longest_word, second_longest_word
+
+import time
+
+# Step 1: Read the input file and store the words
+def read_words_from_file(file_name):
+    with open(file_name, 'r') as file:
+        words = [line.strip() for line in file]
+
+    return
+
+input_file = 'Input_01.txt'
 words = read_words_from_file(input_file)
 
 # Step 2: Sort the words in descending order of length
